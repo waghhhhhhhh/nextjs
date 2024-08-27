9f8da50def9085d8f27f742184a26ec444984ce7
lxj <lxj115733@gmail.com>
2024-08-27 18:19:16 +1000

    fixing git's bugs

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -9,6 +9,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
+
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
